00d51d7b25b76f30bbd6b8e1a80bad8d208c80c6
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-02-10 10:57:32 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
+echo "FILE_DATE=$(date +%Y%m%d%H%M)" >>"$GITHUB_ENV"
